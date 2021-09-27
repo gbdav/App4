a2ef4794634aa7bd00cd2368ca038eb1a1e46977
David <2018371056@uteq.edu.mx>
2021-09-27 00:38:24 -0500

    Commit inicial

---
+++ App4/App4/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App4.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ App4/App4/ViewModels/CountViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App4.ViewModels
+{
+    public class CountViewModel : BaseViewModel
+    {
+        int contador;
+        ICommand buttonClickCommand;
+        ICommand resetClickCommand;
+        string countConverted;
+
+        public CountViewModel()
+        {
+            contador = 0;
+        }
+
+        public int Contador {
+            get => contador;
+            set
+            {
+                if (value == contador) return;
+                contador = value;
+                CountConverted = $"Clickeado {contador} veces";
+                OnPropertyChanged(nameof(CountConverted));
+            }
+        }
+
+        public string CountConverted
+        {
+            get => countConverted;
+            set
+            {
+                if (string.Equals(countConverted, value)) return;
+                countConverted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ButtonClickCommand
+        {
+            get => buttonClickCommand ?? (buttonClickCommand = new Command(ButtonClickAction));
+        }
+
+        public ICommand ResetClickCommand
+        {
+            get => resetClickCommand ?? (resetClickCommand = new Command(ResetClickAction));
+        }
+
+        private void ResetClickAction()
+        {
+            Contador = 0;
+            CountConverted = $"Contador reseteado";
+        }
+
+        private void ButtonClickAction()
+        {
+            Contador++;
+        }
+    }
+}
